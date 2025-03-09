0b3f52bdb3c48d78dcae50ec518e3c8599b123cc
andrearocioperez <rocioarriola1204@outlook.com>
2025-03-08 20:57:40 -0600

    Se edito la clase "CatalogosDbContext" borrando un pedzo de código, que colocamos en otros lugares diferentes que son: el appsetting.json y Program.cs

--- Practica7032025.APPMVDatabaseFirst/Program.cs
+++ Practica7032025.APPMVDatabaseFirst/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Practica7032025.APPMVDatabaseFirst.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CatalogosDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+
 
 var app = builder.Build();
 
